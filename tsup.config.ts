4b1ce6907de140fcd0d888db3b03f16413191fbe
gustavo.henrique <gustavo.henrique@atak.com.br>
2025-09-07 00:15:40 -0300

    refactor: change manifest structure for tests

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'tsup'
+import { cpSync } from 'fs'
 
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm'],
-  dts: {
-    resolve: true,
-  },
+  dts: { resolve: true },
   clean: true,
   sourcemap: true,
   splitting: true,
   tsconfig: './tsconfig.build.json',
+  onSuccess: async () => {
+    cpSync('src/icon.png', 'dist/icon.png')
+  }
 })
